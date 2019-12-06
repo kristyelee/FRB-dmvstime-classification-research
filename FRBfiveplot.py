4c140e25de764f80f1ba9ff840f6b928717f7f73
Kristy Lee <kristylee@berkeley.edu>
2019-12-05 21:16:31 -0800

    Five FRB Freq. Vs. Time and Dm Vs. Time Plot

--- FRBfiveplot.py
+++ FRBfiveplot.py
@@ -32,6 +32,8 @@ def generate_five(spectra_array, classification_labels):
         if classification_labels[i] == 1:
             plt.imshow(spectra_array[i].data, aspect='auto')
             plt.show()
+            print(i)
+            print(spectra_array[i].freqs.max(), spectra_array[i].freqs.min(), spectra_array[i].dm)
             dmvstm_array = create_dmvstime_array(spectra_array[i])
             plt.imshow(dmvstm_array, aspect='auto')
             plt.show()
