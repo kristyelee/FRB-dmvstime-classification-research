6b8e010ac4e610369d3801b517f06e0a2e286ce6
Kristy Lee <kristylee@berkeley.edu>
2019-12-07 17:16:20 -0800

    Updated plots + confusion matrix

--- FRBfiveplot.py
+++ FRBfiveplot.py
@@ -34,7 +34,7 @@ def generate_five(spectra_array, classification_labels):
             print(spectra_array[i].freqs.max(), spectra_array[i].freqs.min(), spectra_array[i].dm)
             dmvstm_array = create_dmvstime_array(spectra_array[i])
             spectra_array[i].dm = 0
-            plt.imshow(np.fliplr(spectra_array[i].data), aspect='auto')
+            plt.imshow(np.fliplr(spectra_array[i].data), aspect='auto', extent=(0, 250, spectra_array[i].freqs.min(), spectra_array[i].freqs.max()))
             plt.show()
             plt.imshow(dmvstm_array, aspect='auto')
             plt.show()
